9f3796d350469328ebd94dcd79ad71505bfb98c9
Phyo Wai Yan Tun <63502848+phorus-stevison@users.noreply.github.com>
2026-01-28 10:25:56 +0630

    Update default userID to a new UUID

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '55a9de81-8f5b-4a2a-8c17-af1a3ccdca83'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
